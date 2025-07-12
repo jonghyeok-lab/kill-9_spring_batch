af69e1e12243dc06d0f9c099f95256b376695a35
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-07-13 01:02:40 +0900

    초기 커밋

---
+++ src/main/java/batch/kill9/Kill9Application.java
@@ -0,0 +1,13 @@
+package batch.kill9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kill9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kill9Application.class, args);
+    }
+
+}
---
+++ src/test/java/batch/kill9/Kill9ApplicationTests.java
@@ -0,0 +1,13 @@
+package batch.kill9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kill9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
