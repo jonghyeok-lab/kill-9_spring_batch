f9f60921619c3d1337dd9d62b1eea340570810a3
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-08-18 09:10:57 +0900

    FlatFileItemReader- record & PatternMatching

---
+++ src/main/java/batch/kill9/chapter2/file/RecordConfig.java
@@ -0,0 +1,34 @@
+package batch.kill9.chapter2.file;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+
+public class RecordConfig {
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<SystemDeath> reader(
+            @Value("#{jobParameters['inputFile']}") String inputFile
+    ) {
+        return new FlatFileItemReaderBuilder<SystemDeath>()
+                .name("systemKill")
+                .resource(new FileSystemResource(inputFile))
+                .delimited()
+                .delimiter(",")
+                .names("command", "cpu", "status")
+                .targetType(SystemDeath.class) // record 전달 시, RecordFieldSetMapper 를 사용
+                .linesToSkip(1)
+                .build();
+    }
+
+
+    public static record SystemDeath(
+            String command,
+            int cpu,
+            String status
+    ) {}
+}
